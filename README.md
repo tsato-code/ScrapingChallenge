b0e6d77a2e0d55733e69d88443d513b7013f8fdc
tsato <suika.1106@gmail.com>
2020-03-07 00:40:35 +0900

    add picture

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Scraping Challenge
 - [sumaity](https://sumaity.com/)
 - [cookpad](https://cookpad.com/)
+
+<div align="center">
+<img src="docs/fig01.png" width=650pt>
+</div>
--- cookpad/crawling.py
+++ cookpad/crawling.py
@@ -38,7 +38,7 @@ def crawling(args):
     # 総レシピ数の取得
     soup = BeautifulSoup(response.content, "lxml")
     num_recipe = int(soup.find(class_="paginator").contents[1].text.split("/")[1].strip().replace(",", ""))
-    print(f"総レシピ数： {num_recipe}")
+    logger.info(f"総レシピ数： {num_recipe}")
 
     # テキストファイルにキャラ弁数を保存
     with open(os.path.join(OUT_DIR, "data.txt"), "w") as f:
@@ -50,7 +50,7 @@ def crawling(args):
         next_url = soup.find("a", rel="next")
 
         if next_url is None:
-            print(f"総ページ数： {page_count-1}")
+            logger.info(f"総ページ数： {page_count-1}")
             with open(os.path.join(OUT_DIR, "data.txt"), mode="a") as f:
                 f.write(f"{page_count-1}\n")
             break
@@ -58,13 +58,13 @@ def crawling(args):
         url = urllib.parse.urljoin(HOME_URL, next_url.attrs["href"])
 
         # 取得できるまで繰り返し
-        for _i in range(1000):
+        for _i in range(360):
             try:
                 response = requests.get(url)
                 time.sleep(1)
                 break
             except Exception as e:
-                print(e)
+                logger.info(e)
                 time.sleep(10)
 
         with open(os.path.join(OUT_DIR, f"page{page_count:000006}.html"), "w", encoding="utf-8") as f:
@@ -72,12 +72,12 @@ def crawling(args):
 
         soup = BeautifulSoup(response.content, "lxml")
         if page_count % 10 == 0:
-            print(f"{page_count}ページ取得")
+            logger.info(f"{page_count}ページ取得")
 
 
 def main():
     args = get_args()
-    print(args)
+    logger.info(args)
     os.makedirs(args.out_dir, exist_ok=True)
     crawling(args)
 
@@ -110,7 +110,7 @@ if __name__ == "__main__":
     logger.addHandler(handler)
 
     date_now = datetime.datetime.now()
-    print(f"クローリング開始: {date_now}")
+    logger.info(f"クローリング開始: {date_now}")
     main()
     date_now = datetime.datetime.now()
-    print(f"クローリング終了: {date_now}")
+    logger.info(f"クローリング終了: {date_now}")
